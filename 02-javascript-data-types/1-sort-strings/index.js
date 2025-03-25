4e50fc971e72d4f4d2807a5e2ff4486ab586a20d
valeron <valeronchik0702@gmail.com>
2025-03-25 14:44:18 +0300

    added sort function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,25 +1,21 @@
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
- * @param {object} [param="asc"] param - the sorting type "asc" or "desc"
+ * @param {object} param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, params = {
-  sort: "asc",
-  localeName: "en",
-  sensitivity: 'base',
-}) {
+export function sortStrings(arr, param = 'asc') {
   arr.sort((a, b) => {
-    if (a.localeCompare(b, params.localeName, {sensitivity: params.sensitivity}) === 1) {
+    if (a.localeCompare(b, ['ru', 'en'], { caseFirst: "upper" }) === 1) {
       return 1;
     }
 
-    if (a.localeCompare(b, 'ru') === 0) {
+    if (a.localeCompare(b, ['ru', 'en'], { caseFirst: "upper" }) === 0) {
       return 0;
     }
 
-    if (a.localeCompare(b, 'ru') === -1) {
-      return 1;
+    if (a.localeCompare(b, ['ru', 'en'], { caseFirst: "upper" }) === -1) {
+      return -1;
     }
   });
 
@@ -29,21 +25,21 @@ export function sortStrings(arr, params = {
 
   return arr;
 }
+/*
 
-//descending
-//ascending
-// const fruits = ['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'];
-// const chars = ['b', 'a', 'c'];
-// console.log(fruits)
-sortStrings(fruits, {sort: "desc", localeName: "ru", sensitivity: 'base'});
-// console.log(fruits)
-// console.log(chars);
-// sortStrings(chars, 'desc');
-// console.log(chars);
+const fruits = ['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'];
+const chars = ['b', 'a', 'c'];
+console.log(fruits)
+sortStrings(fruits, {sort: "asc", localeName: "ru", sensitivity: 'base'});
+console.log(fruits)
+console.log(chars);
+sortStrings(chars, 'asc');
+console.log(chars);
 
 const fruits2 = ['абрикос', 'яблоко', 'ёжик'];
 //['абрикос', 'ёжик', 'яблоко']
-const f = sortStrings(['абрикос', 'яблоко', 'ёжик'], {});
+const f = sortStrings(['абрикос', 'яблоко', 'ёжик']);
 console.log(f);
 
 
+*/
