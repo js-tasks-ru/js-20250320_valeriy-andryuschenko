5dab031063df32574e3d951a3d47b069008e5b4b
valeron <valeronchik0702@gmail.com>
2025-03-28 17:10:44 +0300

    notification messages

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,41 @@
 export default class NotificationMessage {
+  outerElement = document.createElement("div");
+  element = document.createElement("div");
+  constructor(message = '', timer = {}) {
+    this.duration = timer.duration || 2000;
+    this.type = timer.type || 'error';
+    this.message = message;
+  }
 
+  show() {
+    this.element.innerHTML = this.createTemplate();
+    this.outerElement.append(this.element);
+    document.body.append(this.outerElement);
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.element.remove();
+  }
+
+  createTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
 }
