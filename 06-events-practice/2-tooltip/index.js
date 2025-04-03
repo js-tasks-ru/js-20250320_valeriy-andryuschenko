30e78e35170de131902ee59b2d30dd8ac60343f3
valeron <valeronchik0702@gmail.com>
2025-04-03 15:45:28 +0300

    tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,52 @@
 class Tooltip {
-  initialize () {
+  static #toolTipInstance = null;
+  element = null;
 
+  constructor() {
+    this.render();
+    if (!Tooltip.#toolTipInstance) {
+      Tooltip.#toolTipInstance = this;
+    }
+
+    return Tooltip.#toolTipInstance;
   }
+
+  initialize() {
+    document.addEventListener('pointerover', this.tooltipOver.bind(this));
+    document.addEventListener('pointerout', this.tooltipOut.bind(this));
+  }
+
+  tooltipOut() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+    }
+  }
+
+  tooltipOver(event) {
+    if (event.target.dataset.tooltip !== undefined) {
+      this.render(event.target);
+    }
+  }
+
+  destroy() {
+    this.element.remove();
+  }
+
+  render(container = null) {
+    if (!container) {
+      const imgDefault = document.createElement("img");
+      imgDefault.setAttribute("src", "public/tooltip.gif");
+      document.body.append(imgDefault);
+      return;
+    }
+
+    this.element = document.createElement("div");
+    this.element.classList.add("tooltip");
+    this.element.innerText = container.dataset.tooltip;
+    container.append(this.element);
+  }
+
 }
 
 export default Tooltip;
